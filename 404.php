2673598039a5b3b43d66c1e18c28a528493d251b
Laurence (Lou) Mongeau <154012227+doucebaleine@users.noreply.github.com>
2025-03-25 23:19:05 -0400

    s5_c1
    Ajout et personnalisation du CSS de la page 404

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php $erreur_texte = get_theme_mod("erreur_texte","");?>
 
 <?php get_header(); ?>
-    <section class="global">
+    <section class="global__erreur">
         <h1>Oups!</h1>
         <p><?php echo $erreur_texte;?></p>
     </section>
