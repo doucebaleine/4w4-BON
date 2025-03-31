45f4d0e7c9e65c18d58fa87c40d8795f57e0b8a0
Laurence (Lou) Mongeau <154012227+doucebaleine@users.noreply.github.com>
2025-03-31 13:29:55 -0400

    s9_c1 Examen Intra
    Ajout du menu de la page 404

--- 404.php
+++ 404.php
@@ -5,6 +5,11 @@
     <section class="global__erreur">
         <h1><?php echo $erreur_titre;?></h1>
         <p><?php echo $erreur_texte;?></p>
+        <?php wp_nav_menu(array(
+                    "menu" => "menu_404",
+                    "container" => "nav",
+                    "container_class" => "404__menu"
+            )); ?>
     </section>
     <footer></footer>
     <?php get_footer() ?>
