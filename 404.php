3a8f6e605940d3252a964c782af93d32a2bd91e0
Laurence (Lou) Mongeau <154012227+doucebaleine@users.noreply.github.com>
2025-03-31 15:01:44 -0400

    s9_c1 Examen Intra
    Ajout du bouton retour à l'accueil

--- 404.php
+++ 404.php
@@ -4,14 +4,16 @@
 <?php $erreur_background = get_theme_mod('404_background', '');?>
 
 <?php get_header(); ?>
-    <section class="global__erreur" style="background-image: url('<?= $erreur_background ?>')">
+    <section class="global__erreur" style="background-image: url('<?= $erreur_background ?>')" style="object-fit: cover">
         <h1 class="titre_404" style="color: <?= $erreur_couleur ?>"><?php echo $erreur_titre;?></h1>
         <p class="texte_404" style="color: <?= $erreur_couleur ?>"><?php echo $erreur_texte;?></p>
+        <a class="bouton_accueil" href="<?php echo home_url() ?>">Retour à l'accueil</a>
         <?php wp_nav_menu(array(
                     "menu" => "menu_404",
                     "container" => "nav",
                     "container_class" => "menu__404"
             )); ?>
+            
     </section>
     <footer></footer>
     <?php get_footer() ?>
